c5116c498d070b6b64e978679616712a6096f6a3
marcooegg <marco.oegg@gmail.com>
2022-03-10 12:41:35 -0300

    [UPD][send_debo_fields] - changed how pricelist items are sent to better fit receiving model

--- send_debo_fields/models/product_pricelist.py
+++ send_debo_fields/models/product_pricelist.py
@@ -15,17 +15,6 @@ _logger = logging.getLogger(__name__)
 class ProductProduct(models.Model):
     _inherit = "product.pricelist"
 
-    def _get_debo_fields(self) -> dict:
-        lines_debo_fields = []
-        for line in self.item_ids:
-            lines_debo_fields.append(line._get_debo_fields())
-        debo_like_fields = {
-            "DESCRIPCION": "name",
-            "ID_CLIENTE_DEBO": self.env.company.id_debo,
-            "REGLAS" : lines_debo_fields,
-        }
-        return debo_like_fields
-
     def _get_base_endpoint(self):
         config_params = self.env["ir.config_parameter"].sudo()
         proper_config = config_params.get_param("debo_cloud_conector.debo_endpoint")
@@ -40,10 +29,11 @@ class ProductProduct(models.Model):
     def create(self, vals_list):
         res = super().create(vals_list)
         try:
-            data_sender.send_debo_fields(
-                data=res._get_debo_fields(),
-                endpoint=f'{res._get_base_endpoint()}{res._get_final_endpoint()}',
-            )
+            for rule in self.item_ids:
+                data_sender.send_debo_fields(
+                    data=rule._get_debo_fields(),
+                    endpoint=f"{self._get_base_endpoint()}{self._get_final_endpoint()}",
+                )
         except Exception as e:
             _logger.error(e)
             raise Warning(e.args)
@@ -53,27 +43,39 @@ class ProductProduct(models.Model):
         res = super().write(vals_list)
         if res:
             try:
-                data_sender.send_debo_fields(
-                    data=self._get_debo_fields(),
-                    endpoint=f'{self._get_base_endpoint()}{self._get_final_endpoint()}',
-                )
+                for rule in self.item_ids:
+                    data_sender.send_debo_fields(
+                        data=rule._get_debo_fields(),
+                        endpoint=f"{self._get_base_endpoint()}{self._get_final_endpoint()}",
+                    )
             except Exception as e:
                 _logger.error(e)
                 raise Warning(e.args)
         return res
-    
+
+
 class ProductPriceListItem(models.Model):
     _inherit = "product.pricelist.item"
 
     def _get_debo_fields(self):
         debo_like_fields = {
-            "CATEGORIA": self.categ_id.id if self.categ_id else False,
+            "DESCRIPCION": self.pricelist_id.name,
+            "ID_CLIENTE_DEBO": self.pricelist_id.env.company.id_debo,
+            "CATEGORIA": self._format_categ_id(),
             "PRODUCTO": self.product_id.id if self.product_id else False,
             "SIGNO": "-" if self.percent_price >= 0 else "+",
             "PORCENTAJE": self.percent_price,
-            "FIJO" : self.fixed_price,
-            "FECHA_VENCIMIENTO": datetime.strftime(self.date_end, data_sender._debo_date_format()),
+            "FIJO": self.fixed_price,
+            "FECHA_VENCIMIENTO": datetime.strftime(
+                self.date_end, data_sender._debo_date_format()
+            ),
             "ID_DEBO_CLOUD": self.id,
             "id_debo": self.id_debo,
         }
         return debo_like_fields
+
+    def _format_categ_id(self):
+        categ_id = self.categ_id
+        if categ_id:
+            return [categ_id.id, categ_id.name]
+        return []
