b92ea742ccc729dbd365da68d021cf7521434a37
marcooegg <marco.oegg@gmail.com>
2022-04-13 08:02:17 -0300

    [UPD][fuel_tanks_cash_control, stock_fuel_tanks] - add id_debo to stock_pump

--- fuel_tanks_cash_control/models/fuel_move_line.py
+++ fuel_tanks_cash_control/models/fuel_move_line.py
@@ -10,6 +10,7 @@ class FuelMoveLine(models.Model):
     id_debo = fields.Char(related="session_id.id_debo", string="Planilla")
 
     pump_id = fields.Many2one(comodel_name="stock.pump", string="Pump")
+    pump_id_debo = fields.Char(related="pump_id.id_debo", string="ID DEBO")
     pump_code = fields.Char(related="pump_id.code", string="Code")
     tank_id = fields.Many2one(comodel_name="stock.location", related="pump_id.tank_id")
     product_id = fields.Many2one(
@@ -17,14 +18,9 @@ class FuelMoveLine(models.Model):
         related="tank_id.product_id",
         string="Product",
     )
-    price = fields.Float(related="product_id.lst_price", string="Price")
+    price = fields.Float(string="Price")
     initial_qty = fields.Float(string="Initial Quantity")
     final_qty = fields.Float(string="Final Quantity")
     manual_qty = fields.Float(string="Manual Quantity")
     cubic_meters = fields.Float(string="Cubic Meters")
-    amount = fields.Float(string="Amount", compute="_compute_amount")
-
-    @api.depends('price','final_qty')
-    def _compute_amount(self):
-        for line in self:
-            line.amount= line.cubic_meters * line.price
+    amount = fields.Float(string="Amount")
