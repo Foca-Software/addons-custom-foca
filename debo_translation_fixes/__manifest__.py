82bd2a196b13f629ddef4f84f235938a52f349ac
marcooegg <marco.oegg@gmail.com>
2022-04-27 08:23:36 -0300

    [ADD][debo_translation_fixes] [UPD][debo_cc_session_spreadsheet, fuel_tanks_cash_control] - add module to handle translations from external modules - fixed some translations

---
+++ debo_translation_fixes/__manifest__.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "DEBO Translation Fixes",
+    "summary": """
+        Translation for several fields requested by DEBO
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["marcooegg"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "application": False,
+    "installable": True,
+    "depends": ["base","cash_control_extension"],
+    ### XML Data files
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+    ###########################
+    # Delete all the commented lines after editing the module
+    ###########################
+}
