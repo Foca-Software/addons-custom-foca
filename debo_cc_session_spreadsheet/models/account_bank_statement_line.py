7cd1c193037078ad13bf9979798cb061a342a40f
marcooegg <marco.oegg@gmail.com>
2022-06-17 20:03:38 -0300

    other way of fixing

--- debo_cc_session_spreadsheet/models/account_bank_statement_line.py
+++ debo_cc_session_spreadsheet/models/account_bank_statement_line.py
@@ -1,5 +1,6 @@
-from odoo import fields, models, api
-
+from odoo import fields, models, api, _
+import logging
+_logger = logging.getLogger(__name__)
 
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
@@ -9,23 +10,29 @@ class AccountBankStatementLine(models.Model):
         for rec in self:
             if rec.transaction_type_cc_select == "TRANSFER_OUT":
                 if rec.amount > 0:
-                    rec.amount_sign = -rec.amount
+                    rec.amount = -rec.amount
             else:
-                rec.amount_sign = abs(rec.amount)
+                rec.amount = abs(rec.amount)
 
     amount_sign = fields.Boolean(compute="_compute_sign_amount", store=True)
 
+    def _transaction_type_cc_select_vals(self):
+        return [
+            ("TRANSFER_OUT", _("Outcome")),
+            ("TRANSFER_IN", _("Income")),
+        ]
+
     transaction_type_cc_select = fields.Selection(
-        [
-            ("TRANSFER_OUT", "Outcome"),
-            ("TRANSFER_IN", "Income"),
-        ],
+        selection=_transaction_type_cc_select_vals,
         string="Transaction Type",
     )
 
     @api.model
     def create(self, vals):
         res = super().create(vals)
-        if res.transaction_type_cc_select:
-            res.transaction_type = res.transaction_type_cc_select
+        if res.transaction_type in [
+            value[0] for value in self._transaction_type_cc_select_vals()
+        ]:
+            _logger.info(self.with_user(1)._fields['transaction_type_cc_select'].selection)
+            res.transaction_type_cc_select = res.transaction_type
         return res
