5cb402f27e26c30a686021fb2115e5d10facd25a
Marco Oegg <marco.oegg@gmail.com>
2022-07-26 18:32:01 -0300

    unresolved conflicts

--- debo_sector/models/stock_location.py
+++ debo_sector/models/stock_location.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-from odoo import models,fields
-
-class StockLocation(models.Model):
-    _inherit = 'stock.location'
-
-    lot_stock_warehouse_id = fields.One2many(comodel_name='stock.warehouse',inverse_name='lot_stock_id',string='Stock Location for Warehouse')
-=======
 from odoo import models, fields
 
 
@@ -17,4 +9,3 @@ class StockLocation(models.Model):
         inverse_name="lot_stock_id",
         string="Stock Location for Warehouse",
     )
->>>>>>> PR_FCO-180.2
